b2d47b3e92e1cc21e475cd59fff35df70bb65e39
Bochoxic <vboscha@gmail.com>
2023-03-15 16:26:40 +0100

    Debugging

--- test_using_opencv.py
+++ test_using_opencv.py
@@ -1,6 +1,8 @@
 import cv2
+import time
 cap = cv2.VideoCapture("rtsp://192.168.144.25:8554/main.264")
 i = 0
+start = time.time()
 while(cap.isOpened()):
     ret, frame = cap.read()
     # cv2.imshow('frame', frame)
@@ -11,3 +13,5 @@ while(cap.isOpened()):
         break
 cap.release()
 cv2.destroyAllWindows()
+end = time.time()
+print(start-end)
