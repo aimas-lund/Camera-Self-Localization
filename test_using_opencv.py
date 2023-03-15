68eb725f4182d9f88142bcedc24fd5e8176bee26
Bochoxic <vboscha@gmail.com>
2023-03-15 15:29:13 +0100

    Debugging

--- test_using_opencv.py
+++ test_using_opencv.py
@@ -1,9 +1,11 @@
 import cv2
 cap = cv2.VideoCapture("rtsp://192.168.144.25:8554/main.264")
-
+i = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
+    i = i + 1
+    print(i)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         print("no cap")
         break
