f954fe5ef5a58a75e4ae4cd29fc530e05f127a9e
Bochoxic <vboscha@gmail.com>
2023-03-15 16:00:18 +0100

    Debugging

--- test_using_opencv.py
+++ test_using_opencv.py
@@ -3,7 +3,7 @@ cap = cv2.VideoCapture("rtsp://192.168.144.25:8554/main.264")
 i = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    # cv2.imshow('frame', frame)
     i = i + 1
     print(i)
     if cv2.waitKey(20) & 0xFF == ord('q'):
