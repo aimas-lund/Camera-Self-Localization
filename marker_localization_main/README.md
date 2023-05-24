ef980259efdbfe9f2f695498557b1970e006635a
bochoxic <vboscha@gmail.com>
2023-05-24 09:37:00 +0200

    Remove folders

--- marker_localization_main/README.md
+++
@@ -1,2 +0,0 @@
-# marker_localization
- 
--- marker_localization_main/src/localization/config.py
+++
@@ -1,4 +0,0 @@
-# This was in the simulation code, but it is from too long ago to know if it is correct
-SIMULATION_RX_OFFSET = 0.1
-SIMULATION_RY_OFFSET = 0.1
-SIMULATION_RZ_OFFSET = 0.1
